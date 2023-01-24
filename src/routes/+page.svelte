9d9b54fb3c6e6dcd1e71c355e6818434c94a7ccf
Tony Ma <tonyphma@gmail.com>
2023-01-24 10:19:58 -0800

    Add barebones logging page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const load = (async () => {
+        const response =  await fetch('/api/autoRefresh/logs')
+        const data = await response.json()
+        return data
+    })()
+</script>
+
+<div>
+{#await load}
+    <p>Loading...</p>
+{:then logs}
+    <table>
+        <tr>
+            <th>Date</th>
+            <th>Test</th>
+            <th>Result</th>
+        </tr>
+    {#each logs.reverse() as { date, name, result }}
+        <tr>
+            <td>{(new Date(date)).toISOString().split('T')[0]}</td>
+            <td>{name}</td>
+            <td>{#if result === 1}✅{:else}❌{/if}</td>
+        </tr>
+    {/each}
+    </table>
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
+</div>
+
+<style>
+    * {
+        text-align: center;
+    }
+    
+    table {
+        border-collapse: collapse;
+        margin: auto;
+    }
+
+    table, th, td {
+        border: 1px solid black
+    }
+
+    th, td {
+        padding: 0.5em;
+    }
+</style>
